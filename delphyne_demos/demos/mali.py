af9e5b806ac4052299a0d2812f4b02b7440be9b3
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2022-04-04 16:10:02 -0300

    Adds LoopRoadPedestrianCrosswalk to mali demo. (#41)

--- delphyne_demos/demos/mali.py
+++ delphyne_demos/demos/mali.py
@@ -208,6 +208,16 @@ KNOWN_ROADS = {
         'moving_forward': True,
         'linear_tolerance': 1e-3,
     },
+    'LoopRoadPedestrianCrosswalk': {
+        'description': 'Loop with two virtual pedestrian crosswalks',
+        'file_path': 'odr/LoopRoadPedestrianCrosswalk.xodr',
+        'yaml_file_path': 'odr/LoopRoadPedestrianCrosswalk.yaml',
+        'agent_type': 'RuleRailCar',
+        'lane_id': '1_0_-1',
+        'lane_position': 0.,
+        'moving_forward': True,
+        'linear_tolerance': 1e-3,
+    },
 }
 
 
